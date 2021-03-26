d4b49071b04768202970e7ff8a848fe080662970
Andy Muir <muirandy@yahoo.co.uk>
2021-03-26 18:38:48 +0000

    Fix for maths - average

--- list_math.pl
+++ list_math.pl
@@ -4,4 +4,5 @@ count(Count, [Head|Tail]) :- count(TailCount, Tail), Count is TailCount + 1.
 sum(0, []).
 sum(Total, [Head|Tail]) :- sum(Sum, Tail), Total is Head + Sum.
 
+average(0, []).
 average(Average, List) :- sum(Sum, List), count(Count, List), Average is Sum/Count.
