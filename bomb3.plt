ce828865387b25a5b3067138b092b282a3d3e767
Andy Muir <muirandy@yahoo.co.uk>
2021-03-30 22:49:30 +0100

    Pretty close.

--- bomb3.plt
+++ bomb3.plt
@@ -1,21 +1,45 @@
-:- begin_tests(bomb2).
-:- use_module(bomb2).
+:- begin_tests(bomb3Safe).
+:- use_module(bomb3Safe).
 
-% (Volume,Current),(Volume,Current),Needed,Turns
+test(bFromA) :- bFromA(10,1,10,0,0,1).
+test(bFromA) :- bFromA(5,4,5,4,3,5).
 
-test(zero) :- defuse(1,0, 1,0, 0).
-test(zero) :- defuse(1,1, 1,0, 0).
-test(zero) :- defuse(1,0, 1,1, 0).
-test(zero) :- defuse(1,1, 5,0, 1).
-test(zero) :- defuse(5,0, 1,1, 1).
-test(zero) :- defuse(1,1, 1,1, 2).
+test(aFromB) :- aFromB(10,0,10,1,1,0).
+test(aFromB) :- aFromB(5,4,5,4,5,3).
 
-test(one) :- defuse(1,0, 5,0, 1).
-test(one) :- defuse(5,0, 1,0, 1).
+test(newState) :- isNewState((1,1),[]).
+test(newState) :- isNewState((1,1),[(1,2)]).
+test(newState) :- \+ isNewState((1,2),[(1,2)]).
 
-%test(two) :- defuse(1,0, 1,1, 2).
-%test(two) :- defuse(1,1, 1,0, 2).
+test(notFull) :- \+ isNotFull(1,1).
+test(notFull) :- isNotFull(2,1).
 
-%test(two) :- defuse(1,0, 1,0, 2).
+test(alreadySolved) :- alreadySolved(1,0,1).
+test(alreadySolved) :- alreadySolved(0,1,1).
+test(alreadySolved) :- alreadySolved(1,1,1).
+test(alreadySolved) :- alreadySolved(1,1,2).
+test(alreadySolved) :- \+ alreadySolved(1,1,3).
+test(isNotSolvedAlready) :- \+ isNotSolvedAlready(1,0,1).
+test(isNotSolvedAlready) :- \+ isNotSolvedAlready(0,1,1).
+test(isNotSolvedAlready) :- \+ isNotSolvedAlready(1,1,1).
+test(isNotSolvedAlready) :- \+ isNotSolvedAlready(1,1,2).
+test(isNotSolvedAlready) :- isNotSolvedAlready(1,1,3).
 
-:- end_tests(bomb2).
+% (Volume,Current),(Volume,Current),Needed,Turns,History
+
+test(zero) :- defuse(1,0, 1,0, 0, []).
+test(zero) :- defuse(1,1, 1,0, 0, []).
+test(zero) :- defuse(1,0, 1,1, 0, []).
+test(zero) :- defuse(1,1, 5,0, 1, []).
+test(zero) :- defuse(5,0, 1,1, 1, []).
+test(zero) :- defuse(1,1, 1,1, 2, []).
+
+test(one) :- defuse(1,0, 5,0, 1, []).
+test(one) :- defuse(5,0, 1,0, 1, []).
+
+test(two) :- defuse(1,0, 1,1, 2, []).
+test(two) :- defuse(1,1, 1,0, 2, []).
+
+test(two) :- defuse(1,0, 1,0, 2, []).
+
+:- end_tests(bomb3Safe).
--- bomb3Safe.pl
+++ bomb3Safe.pl
@@ -1,20 +1,34 @@
-:- module(bomb2, [defuse/6, isNewState/2]).
+:- module(bomb3Safe, [
+  defuse/6,
+  isNotFull/2,
+  alreadySolved/3,
+  isNotSolvedAlready/3,
+  isNewState/2,
+  aFromB/6,
+  bFromA/6]).
 
 use_module(library(lists)).
 
 count(0, []).
 count(Count, [Head|Tail]) :- count(TailCount, Tail), Count is TailCount + 1.
 
-% (Volume,Current),(Volume,Current),Needed
+% (Volume,Current),(Volume,Current),Needed,History
 
 writeSteps([]).
 writeSteps([Head|Tail]) :-
   write(Head), nl,
   writeSteps(Tail).
 
+isEmpty(A,B) :-
+  A =:= 0,
+  B =:= 0.
+
+isOldState(A,B) :-
+  memberchk(A, B).
 isNewState(A,B) :-
   %write('Checking: '), write(A), write(' is in: '), write(B), nl,
-  \+ memberchk(A, B).
+  \+ isOldState(A,B).
+
 
 % Status
 isNotFull(A,B) :- A > B.
@@ -23,36 +37,42 @@ fillA(A,B) :- write('Fill A'), nl.
 fillB(C,D) :- write('Fill B'), nl.
 
 % Empty
-emptyA() :- write('Empty A'), nl.
-emptyB() :- write('Empty B'), nl.
+emptyA(). % :- write('Empty A'), nl.
+emptyB(). % :- write('Empty B'), nl.
 
-bFromA(A,B,C,D,X,Y) :-
+bFromA(_,B,C,D,X,Y) :-
   LIQUID is B + D,
   % B can hold all liquid
   LIQUID =< C,
   X is 0,
   Y is LIQUID.
 
-bFromA(A,B,C,D,X,Y) :-
+bFromA(_,B,C,D,X,Y) :-
   LIQUID is B + D,
   % B cannot hold all liquid
   LIQUID > C,
   X is LIQUID - C,
   Y is LIQUID - X.
 
-aFromB(A,B,C,D,X,Y) :-
+aFromB(A,B,_,D,X,Y) :-
   LIQUID is B + D,
   % A can hold all liquid
   LIQUID =< A,
   Y is 0,
   X is LIQUID.
 
-aFromB(A,B,C,D,X,Y) :-
+aFromB(A,B,_,D,X,Y) :-
   LIQUID is B + D,
   % A cannot hold all liquid
   LIQUID > A,
   Y is LIQUID - A,
   X is LIQUID - Y.
+
+alreadySolved(B,D,E) :-
+  B is E; D is E; E is B + D.
+
+isNotSolvedAlready(B,D,E) :-
+  \+ alreadySolved(B,D,E).
 
 % Defused! Success!!
 defuse(_,_, _,_, 0, _) :- write('Nothing to Defuse!'), nl.
@@ -65,8 +85,9 @@ defuse(_,B, _,D, E, H) :-
 
 % Fill A
 defuse(A,B, C,D, E, H) :-
+  isNotSolvedAlready(B,D,E),
   A =\= B,
-  isNotFull(C,D),
+  isNotFull(A,B),
   isNewState((A,D), H),
   fillA(A,B),
   append([(A,D)], H, NEW_H),
@@ -74,8 +95,9 @@ defuse(A,B, C,D, E, H) :-
 
 % Fill B
 defuse(A,B, C,D, E, H) :-
+  isNotSolvedAlready(B,D,E),
   C =\= D,
-  isNotFull(A,B),
+  isNotFull(C,D),
   isNewState((B,C), H),
   fillB(C,D),
   append([(B,C)], H, NEW_H),
@@ -83,23 +105,27 @@ defuse(A,B, C,D, E, H) :-
 
 % Fill B from A
 defuse(A,B, C,D, E, H) :-
-  %\+ B is E; \+ D is E; \+ E is B + D,
+  B > 0,
+  isNotSolvedAlready(B,D,E),
   bFromA(A,B,C,D,X,Y),
   isNewState((X,Y), H),
-  write('Fill B from A'), nl,
+  %write('Fill B from A'), nl,
   append([(X,Y)], H, NEW_H),
   defuse(A,X,C,Y,E,NEW_H).
 
 % Fill A from B
 defuse(A,B, C,D, E, H) :-
+  D > 0,
+  isNotSolvedAlready(B,D,E),
   aFromB(A,B,C,D,X,Y),
   isNewState((X,Y), H),
-  write('Fill A from B'), nl,
+  %write('Fill A from B'), nl,
   append([(X,Y)], H, NEW_H),
   defuse(A,X,C,Y,E,NEW_H).
 
 % Empty A
 defuse(A,B, C,D, E, H) :-
+  isNotSolvedAlready(B,D,E),
   B > 0,
   isNewState((0,D), H),
   emptyA(),
@@ -108,8 +134,9 @@ defuse(A,B, C,D, E, H) :-
 
 % Empty B
 defuse(A,B, C,D, E, H) :-
+  isNotSolvedAlready(B,D,E),
   D > 0,
   isNewState((B,0), H),
   emptyB(),
   append([(B,0)], H, NEW_H),
-  defuse(A,B, C,0,E, NEW_H).
+  defuse(A,B, C,0, E, NEW_H).
